8d0442bee2a8ca9b390c13af0d3b1e174f3f3743
rlss91 <rlss91@gmail.com>
2022-01-16 20:52:09 +0200

    fragment

--- src/app/pages/petsnamepage/petsnamepage.component.ts
+++ src/app/pages/petsnamepage/petsnamepage.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-petsnamepage',
   templateUrl: './petsnamepage.component.html',
-  styleUrls: ['./petsnamepage.component.css']
+  styleUrls: ['./petsnamepage.component.css'],
 })
 export class PetsnamepageComponent implements OnInit {
+  selectedPet: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.selectedPet = '';
   }
 
+  ngOnInit(): void {
+    // console.log(this.route.snapshot.queryParams);
+    this.route.queryParams.subscribe((data: any) => {
+      console.log(data);
+      this.selectedPet = data.petname;
+    });
+    this.route.fragment.subscribe((data: any) => {
+      console.log('fragment:', data);
+    });
+  }
 }
