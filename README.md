9d17041636417eb3fca96c2067c2ae33c375c241
rlss91 <rlss91@gmail.com>
2022-02-02 21:12:04 +0200

    hw

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## home work 1
+
+create auth service
+btn login, logout
+create authguard to protect the routes except home page, login page
+when user login take it to dashboard page
+dashboard page:
+list name page,
+you can look in the list for name that you will pass using queryParams
--- src/app/pages/servernames/servernames.component.ts
+++ src/app/pages/servernames/servernames.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servernames',
   templateUrl: './servernames.component.html',
-  styleUrls: ['./servernames.component.css']
+  styleUrls: ['./servernames.component.css'],
 })
 export class ServernamesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.url.subscribe((data) => {
+      console.log('data', data);
+    });
   }
-
 }
